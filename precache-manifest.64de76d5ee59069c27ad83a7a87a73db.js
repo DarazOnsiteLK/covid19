5c3aec8f5f291268a5eafb04fa9a95fdebbbf8ae
DarazOnsiteLK <52481800+DarazOnsiteLK@users.noreply.github.com>
2020-04-09 22:09:47 +0530

    Add files via upload

---
+++ precache-manifest.64de76d5ee59069c27ad83a7a87a73db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afee57afa80af524e93af658def887bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "974eaba9ce7d91921194",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "0ac0b2dc762b3635b251",
+    "url": "/static/css/main.eaae1270.chunk.css"
+  },
+  {
+    "revision": "974eaba9ce7d91921194",
+    "url": "/static/js/2.eae01686.chunk.js"
+  },
+  {
+    "revision": "b4cabae32341920cfb4131dbdebda2c2",
+    "url": "/static/js/2.eae01686.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ac0b2dc762b3635b251",
+    "url": "/static/js/main.218db834.chunk.js"
+  },
+  {
+    "revision": "f5d8775125235f7bde46",
+    "url": "/static/js/runtime-main.9807545b.js"
+  }
+]);
